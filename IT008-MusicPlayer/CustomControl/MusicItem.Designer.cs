81427151f379f4fd4ed437a81736d9f71c43ae1a
NgocHai <79315800+letruongngochai@users.noreply.github.com>
2023-02-01 12:52:00 +0700

    dev: design LibraryScreen and MusicItem control

---
+++ IT008-MusicPlayer/CustomControl/MusicItem.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace IT008_MusicPlayer.CustomControl
+{
+    partial class MusicItem
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.iconButton1 = new FontAwesome.Sharp.IconButton();
+            this.musicAuthor = new System.Windows.Forms.Label();
+            this.musicName = new System.Windows.Forms.Label();
+            this.musicPicture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.musicPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // iconButton1
+            // 
+            this.iconButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.iconButton1.FlatAppearance.BorderSize = 0;
+            this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.Play;
+            this.iconButton1.IconColor = System.Drawing.Color.Black;
+            this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton1.IconSize = 32;
+            this.iconButton1.Location = new System.Drawing.Point(187, 169);
+            this.iconButton1.Name = "iconButton1";
+            this.iconButton1.Size = new System.Drawing.Size(40, 40);
+            this.iconButton1.TabIndex = 10;
+            this.iconButton1.UseVisualStyleBackColor = true;
+            // 
+            // musicAuthor
+            // 
+            this.musicAuthor.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.musicAuthor.AutoSize = true;
+            this.musicAuthor.Font = new System.Drawing.Font("Cascadia Mono", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.musicAuthor.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.musicAuthor.Location = new System.Drawing.Point(4, 290);
+            this.musicAuthor.Name = "musicAuthor";
+            this.musicAuthor.Size = new System.Drawing.Size(70, 22);
+            this.musicAuthor.TabIndex = 9;
+            this.musicAuthor.Text = "Author";
+            // 
+            // musicName
+            // 
+            this.musicName.AutoSize = true;
+            this.musicName.Font = new System.Drawing.Font("Cascadia Mono", 12F);
+            this.musicName.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.musicName.Location = new System.Drawing.Point(4, 231);
+            this.musicName.MaximumSize = new System.Drawing.Size(240, 0);
+            this.musicName.Name = "musicName";
+            this.musicName.Size = new System.Drawing.Size(60, 27);
+            this.musicName.TabIndex = 8;
+            this.musicName.Text = "Name";
+            // 
+            // musicPicture
+            // 
+            this.musicPicture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.musicPicture.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.musicPicture.Location = new System.Drawing.Point(12, 7);
+            this.musicPicture.Name = "musicPicture";
+            this.musicPicture.Size = new System.Drawing.Size(224, 211);
+            this.musicPicture.TabIndex = 7;
+            this.musicPicture.TabStop = false;
+            // 
+            // MusicItem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.Controls.Add(this.iconButton1);
+            this.Controls.Add(this.musicAuthor);
+            this.Controls.Add(this.musicName);
+            this.Controls.Add(this.musicPicture);
+            this.Name = "MusicItem";
+            this.Size = new System.Drawing.Size(246, 319);
+            ((System.ComponentModel.ISupportInitialize)(this.musicPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private FontAwesome.Sharp.IconButton iconButton1;
+        public System.Windows.Forms.Label musicAuthor;
+        public System.Windows.Forms.Label musicName;
+        public System.Windows.Forms.PictureBox musicPicture;
+    }
+}
---
+++ IT008-MusicPlayer/LibraryScreen.Designer.cs
@@ -0,0 +1,306 @@
+﻿
+namespace IT008_MusicPlayer
+{
+    partial class LibraryScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LibraryScreen));
+            this.pbRemix = new System.Windows.Forms.PictureBox();
+            this.iconButton8 = new FontAwesome.Sharp.IconButton();
+            this.pbPop = new System.Windows.Forms.PictureBox();
+            this.iconButton7 = new FontAwesome.Sharp.IconButton();
+            this.pbRap = new System.Windows.Forms.PictureBox();
+            this.iconButton6 = new FontAwesome.Sharp.IconButton();
+            this.pbVietNam = new System.Windows.Forms.PictureBox();
+            this.iconButton5 = new FontAwesome.Sharp.IconButton();
+            this.pbAll = new System.Windows.Forms.PictureBox();
+            this.iconButton3 = new FontAwesome.Sharp.IconButton();
+            this.pbUSUK = new System.Windows.Forms.PictureBox();
+            this.iconButton2 = new FontAwesome.Sharp.IconButton();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRemix)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRap)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbVietNam)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAll)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbUSUK)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbRemix
+            // 
+            this.pbRemix.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.pbRemix.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbRemix.BackgroundImage")));
+            this.pbRemix.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbRemix.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbRemix.Location = new System.Drawing.Point(1194, 74);
+            this.pbRemix.Name = "pbRemix";
+            this.pbRemix.Size = new System.Drawing.Size(50, 50);
+            this.pbRemix.TabIndex = 33;
+            this.pbRemix.TabStop = false;
+            // 
+            // iconButton8
+            // 
+            this.iconButton8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.iconButton8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.iconButton8.FlatAppearance.BorderSize = 0;
+            this.iconButton8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton8.Font = new System.Drawing.Font("Cascadia Mono", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iconButton8.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.iconButton8.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.iconButton8.IconColor = System.Drawing.Color.Black;
+            this.iconButton8.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton8.Location = new System.Drawing.Point(1250, 74);
+            this.iconButton8.Name = "iconButton8";
+            this.iconButton8.Size = new System.Drawing.Size(110, 51);
+            this.iconButton8.TabIndex = 32;
+            this.iconButton8.Text = "Remix";
+            this.iconButton8.UseVisualStyleBackColor = false;
+            // 
+            // pbPop
+            // 
+            this.pbPop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.pbPop.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbPop.BackgroundImage")));
+            this.pbPop.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbPop.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbPop.Location = new System.Drawing.Point(1063, 74);
+            this.pbPop.Name = "pbPop";
+            this.pbPop.Size = new System.Drawing.Size(50, 50);
+            this.pbPop.TabIndex = 31;
+            this.pbPop.TabStop = false;
+            // 
+            // iconButton7
+            // 
+            this.iconButton7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.iconButton7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.iconButton7.FlatAppearance.BorderSize = 0;
+            this.iconButton7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton7.Font = new System.Drawing.Font("Cascadia Mono", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iconButton7.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.iconButton7.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.iconButton7.IconColor = System.Drawing.Color.Black;
+            this.iconButton7.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton7.Location = new System.Drawing.Point(1119, 74);
+            this.iconButton7.Name = "iconButton7";
+            this.iconButton7.Size = new System.Drawing.Size(82, 51);
+            this.iconButton7.TabIndex = 30;
+            this.iconButton7.Text = "Pop";
+            this.iconButton7.UseVisualStyleBackColor = false;
+            // 
+            // pbRap
+            // 
+            this.pbRap.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.pbRap.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbRap.BackgroundImage")));
+            this.pbRap.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbRap.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbRap.Location = new System.Drawing.Point(921, 74);
+            this.pbRap.Name = "pbRap";
+            this.pbRap.Size = new System.Drawing.Size(50, 50);
+            this.pbRap.TabIndex = 29;
+            this.pbRap.TabStop = false;
+            // 
+            // iconButton6
+            // 
+            this.iconButton6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.iconButton6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.iconButton6.FlatAppearance.BorderSize = 0;
+            this.iconButton6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton6.Font = new System.Drawing.Font("Cascadia Mono", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iconButton6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.iconButton6.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.iconButton6.IconColor = System.Drawing.Color.Black;
+            this.iconButton6.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton6.Location = new System.Drawing.Point(977, 74);
+            this.iconButton6.Name = "iconButton6";
+            this.iconButton6.Size = new System.Drawing.Size(82, 51);
+            this.iconButton6.TabIndex = 28;
+            this.iconButton6.Text = "Rap";
+            this.iconButton6.UseVisualStyleBackColor = false;
+            // 
+            // pbVietNam
+            // 
+            this.pbVietNam.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.pbVietNam.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbVietNam.BackgroundImage")));
+            this.pbVietNam.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbVietNam.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbVietNam.Location = new System.Drawing.Point(721, 74);
+            this.pbVietNam.Name = "pbVietNam";
+            this.pbVietNam.Size = new System.Drawing.Size(50, 50);
+            this.pbVietNam.TabIndex = 27;
+            this.pbVietNam.TabStop = false;
+            // 
+            // iconButton5
+            // 
+            this.iconButton5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.iconButton5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.iconButton5.FlatAppearance.BorderSize = 0;
+            this.iconButton5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton5.Font = new System.Drawing.Font("Cascadia Mono", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iconButton5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.iconButton5.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.iconButton5.IconColor = System.Drawing.Color.Black;
+            this.iconButton5.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton5.Location = new System.Drawing.Point(777, 74);
+            this.iconButton5.Name = "iconButton5";
+            this.iconButton5.Size = new System.Drawing.Size(138, 51);
+            this.iconButton5.TabIndex = 26;
+            this.iconButton5.Text = "VietNam";
+            this.iconButton5.UseVisualStyleBackColor = false;
+            // 
+            // pbAll
+            // 
+            this.pbAll.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.pbAll.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbAll.BackgroundImage")));
+            this.pbAll.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbAll.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbAll.Location = new System.Drawing.Point(398, 75);
+            this.pbAll.Name = "pbAll";
+            this.pbAll.Size = new System.Drawing.Size(50, 50);
+            this.pbAll.TabIndex = 25;
+            this.pbAll.TabStop = false;
+            // 
+            // iconButton3
+            // 
+            this.iconButton3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.iconButton3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.iconButton3.FlatAppearance.BorderSize = 0;
+            this.iconButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton3.Font = new System.Drawing.Font("Cascadia Mono", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iconButton3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.iconButton3.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.iconButton3.IconColor = System.Drawing.Color.Black;
+            this.iconButton3.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton3.Location = new System.Drawing.Point(454, 75);
+            this.iconButton3.Name = "iconButton3";
+            this.iconButton3.Size = new System.Drawing.Size(77, 51);
+            this.iconButton3.TabIndex = 24;
+            this.iconButton3.Text = "All";
+            this.iconButton3.UseVisualStyleBackColor = false;
+            // 
+            // pbUSUK
+            // 
+            this.pbUSUK.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.pbUSUK.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbUSUK.BackgroundImage")));
+            this.pbUSUK.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbUSUK.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbUSUK.Location = new System.Drawing.Point(543, 74);
+            this.pbUSUK.Name = "pbUSUK";
+            this.pbUSUK.Size = new System.Drawing.Size(50, 50);
+            this.pbUSUK.TabIndex = 23;
+            this.pbUSUK.TabStop = false;
+            // 
+            // iconButton2
+            // 
+            this.iconButton2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.iconButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.iconButton2.FlatAppearance.BorderSize = 0;
+            this.iconButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton2.Font = new System.Drawing.Font("Cascadia Mono", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iconButton2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.iconButton2.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.iconButton2.IconColor = System.Drawing.Color.Black;
+            this.iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton2.Location = new System.Drawing.Point(599, 74);
+            this.iconButton2.Name = "iconButton2";
+            this.iconButton2.Size = new System.Drawing.Size(116, 51);
+            this.iconButton2.TabIndex = 22;
+            this.iconButton2.Text = "US/UK";
+            this.iconButton2.UseVisualStyleBackColor = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(37, 174);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1500, 625);
+            this.flowLayoutPanel1.TabIndex = 21;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cascadia Mono", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(279, 79);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Library";
+            // 
+            // LibraryScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.ClientSize = new System.Drawing.Size(1522, 750);
+            this.Controls.Add(this.pbRemix);
+            this.Controls.Add(this.iconButton8);
+            this.Controls.Add(this.pbPop);
+            this.Controls.Add(this.iconButton7);
+            this.Controls.Add(this.pbRap);
+            this.Controls.Add(this.iconButton6);
+            this.Controls.Add(this.pbVietNam);
+            this.Controls.Add(this.iconButton5);
+            this.Controls.Add(this.pbAll);
+            this.Controls.Add(this.iconButton3);
+            this.Controls.Add(this.pbUSUK);
+            this.Controls.Add(this.iconButton2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LibraryScreen";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbRemix)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRap)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbVietNam)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAll)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbUSUK)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbRemix;
+        private FontAwesome.Sharp.IconButton iconButton8;
+        private System.Windows.Forms.PictureBox pbPop;
+        private FontAwesome.Sharp.IconButton iconButton7;
+        private System.Windows.Forms.PictureBox pbRap;
+        private FontAwesome.Sharp.IconButton iconButton6;
+        private System.Windows.Forms.PictureBox pbVietNam;
+        private FontAwesome.Sharp.IconButton iconButton5;
+        private System.Windows.Forms.PictureBox pbAll;
+        private FontAwesome.Sharp.IconButton iconButton3;
+        private System.Windows.Forms.PictureBox pbUSUK;
+        private FontAwesome.Sharp.IconButton iconButton2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
