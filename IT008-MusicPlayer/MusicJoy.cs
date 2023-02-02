a187d9c2ccafcb5cbb8dbe4b526840b5cab7ace1
NgocHai <79315800+letruongngochai@users.noreply.github.com>
2023-02-02 17:07:10 +0700

    dev: define a class to load a child form in parent form

--- IT008-MusicPlayer/MusicJoy.cs
+++ IT008-MusicPlayer/MusicJoy.cs
@@ -8,13 +8,33 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
 namespace IT008_MusicPlayer
 {
     public partial class MusicJoy : Form
     {
+        private Form activeForm = null;
         public MusicJoy()
         {
             InitializeComponent();
+            Variables.ListFormPanel.ListFormsPanel.Add(mainPanel);
         }
+        public void LoadChildForm(Form childForm)
+        {
+            if (activeForm != null)
+                activeForm.Close();
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+
+            Variables.ListFormPanel.ListFormsPanel[0].Controls.Add(childForm);
+            Variables.ListFormPanel.ListFormsPanel[0].Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+
+
     }
 }
---
+++ IT008-MusicPlayer/Variables/ListFormPanel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IT008_MusicPlayer.Variables
+{
+    class ListFormPanel
+    {
+        private static List<Panel> listFormsPanel = new List<Panel>();
+        public static List<Panel> ListFormsPanel { get => listFormsPanel; set => listFormsPanel = value; }
+    }
+}
